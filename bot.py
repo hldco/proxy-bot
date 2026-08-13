2b7a4510bf04b348c6ff6c05576f46dc429f41cb
hldco <hldco98@gmail.com>
2026-08-13 15:17:41 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 CHANNEL_USERNAME = "@goololgoo"
 
 # منبع اول: گیت‌هاب (برای V2ray)
-SOURCE_URL = "https://raw.githubusercontent.com/barry-far/V2ray-config/main/Sub2.txt"
+SOURCE_URL = "https://raw.githubusercontent.com/fifthidea/sub/refs/heads/main/sub/ir.txt"
 
 # منبع دوم: کانال‌های تلگرام (برای MTProto) - ۵ کانال را بدون @ وارد کنید
 SOURCE_CHANNELS = ["PinkProxy", "Myporoxy", "ProxyWR" , "P500Y", "ProxyMTProto"] 
